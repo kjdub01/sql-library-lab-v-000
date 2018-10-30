9c5bb5e5926379f39337fe137fa18ff2b48bea8c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-30 03:13:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.titles, books.year FROM books
-  JOIN series ON books."
+  JOIN series ON books.series_id = series.series_id
+  WHERE"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
