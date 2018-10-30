d45a273fa3d400b2ccd60adbd346bad288eeb3dd
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-30 02:35:44 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO authors (name) VALUES ("Madeleine L'Engle");
+INSERT INTO authors (name) VALUES ("JK Rowling");
+
+INSERT INTO subgenres (name) VALUES ("Christian");
+INSERT INTO subgenres (name) VALUES ("Young Adult");
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Time Quintet", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("A Wrinkle in Time", 1962, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Wind in the Door", 1973, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Swiftly Tilting Planet", 1978, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Philosopher's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Prisoner of Azkaban", 1999, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ("Dobby", "Dobby has no master. Dobby is a free elf!", "House Elf", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ("Hedwig", "Very smart owl you've got there", "Owl", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ("Firenze" "Centaurs are not the servants or playthings of humans.", "Cantaur", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ("Grawp", "HERMY! WHERE HAGGER?" "Giant", 2, 2);
