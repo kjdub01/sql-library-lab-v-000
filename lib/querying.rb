24bf238c954240c5242d065493a384bd724b05cf
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-10-30 03:31:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM series
   JOIN authors ON series.authors_id = authors.id
-  JOIN "
+  JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
